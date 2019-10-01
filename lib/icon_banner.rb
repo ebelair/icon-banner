ddcfe1247d85b1aa8ad31d501b9d7278a5e099e9
Émile Bélair <ebelair@mirego.com>
2019-10-01 18:12:57 -0400

    Bump to v0.1.1

--- lib/icon_banner.rb
+++ lib/icon_banner.rb
@@ -3,7 +3,7 @@ require 'icon_banner/ic_launcher'
 require 'commander'
 
 module IconBanner
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
   DESCRIPTION = 'IconBanner adds custom nice-looking banners over your mobile app icons'
 
   UI = FastlaneCore::UI
