5d0d163046c9be6c2c7bee15a6fd8a656d5d2bfc
Émile Bélair <ebelair@mirego.com>
2019-11-18 16:35:50 -0500

    Fix build path on Android (again)

--- lib/icon_banner/ic_launcher.rb
+++ lib/icon_banner/ic_launcher.rb
@@ -75,7 +75,23 @@ module IconBanner
     end
 
     def backup_path(path)
-      path.gsub('/src/', "/src#{BACKUP_EXTENSION}/")
+      ext = File.extname path
+      backup_path = path
+      backup_path = append_parent_dirname(path, 'res', BACKUP_EXTENSION) if path.include?('/res/')
+      backup_path = path.gsub('/src/', "/src#{BACKUP_EXTENSION}/") if path == backup_path
+      backup_path = path.gsub('/app/', "/src#{BACKUP_EXTENSION}/") if path == backup_path
+      backup_path = path.gsub('/android/', "/src#{BACKUP_EXTENSION}/") if path == backup_path
+      backup_path = path.gsub(ext, BACKUP_EXTENSION + ext) if path == backup_path
+      backup_path
+    end
+
+    def append_parent_dirname(path, dir, suffix)
+      segments = path.split(File::SEPARATOR)
+      dir_index = segments.index(dir)
+      return path unless dir_index && dir_index - 1 >= 0
+
+      parent_segment = segments[dir_index-1]
+      path.gsub("/#{parent_segment}/","/#{parent_segment}#{suffix}/")
     end
 
     def should_ignore_icon(icon)
